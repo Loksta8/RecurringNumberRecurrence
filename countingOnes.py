be7dc744608a0332c11cb0b66c67666b11afff8e
Loksta8 <lyoyo@yahoo.com>
2018-11-26 17:50:37 -0700

    Added comments describing my thought process

--- countingOnes.py
+++ countingOnes.py
@@ -1,12 +1,20 @@
+'''
+Author: Logan Herrera
+Date: 11/26/2018
+This program was created to count the number of recurring ones.
+'''
+#This function keeps track of the recurrences
 def numberOfOnes(n):
 	count = 0
 	count = str(n).count("1")
 	return count
 
+#This function loops from 1 to a value given to it and uses
+#The "numberOfOnes" function for the entire range of numbers
 def countOnes(n):
 	count = 0
 	for i in range(1,n+1):
 		count = count + numberOfOnes(i)
 	return count
-
+#Finally print the results by using the function
 print(countOnes(199980))
